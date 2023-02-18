1793795f58ec84b12b80b19a0284b46bcbf34fd3
Bagy11 <122831906+Bagy11@users.noreply.github.com>
2023-02-18 00:59:16 +0000

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,27 +8,27 @@
  * of 2 numbers
  *
  * Return: 0 for success
- */
+*/
 int main(void)
 {
-  int i, j;
+	int i, j;
 
-  for (i = '0'; i <= '9'; i++)
-    {
-      for (j = '0'; j <= '9'; j++)
+	for (i = '0'; i <= '9'; i++)
 	{
-	  if ((i < j) & (j <= '9'))
-	    {
-	      putchar(i);
-	      putchar(j);
-	      if ((j < '9') | (i < '8'))
+		for (j = '0'; j <= '9'; j++)
 		{
-		  putchar(',');
-		  putchar(' ');
+			if ((i < j) & (j <= '9'))
+			{
+				putchar(i);
+				putchar(j);
+				if ((j < '9') | (i < '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
-	    }
 	}
-    }
-  putchar('\n');
-  return (0);
+	putchar('\n');
+	return (0);
 }
