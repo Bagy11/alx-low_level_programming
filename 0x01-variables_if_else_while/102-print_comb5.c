edf660a73ae938e04f08f02daf36f6e69881ad6a
Bagy11 <122831906+Bagy11@users.noreply.github.com>
2023-02-18 01:01:49 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,31 +8,31 @@
  * digit numbers
  *
  * Return: 0 for success
- */
+*/
 int main(void)
 {
-  int i, j;
+	int i, j;
 
-  for (i = 0; i <= 99; i++)
-    {
-      for (j = 0; j <= 99; j++)
+	for (i = 0; i <= 99; i++)
 	{
-	  if (i < j)
-	    {
-	      putchar((i / 10) + '0');
-	      putchar((i % 10) + '0');
-	      putchar(' ');
-	      putchar((j / 10) + '0');
-	      putchar((j % 10) + '0');
-
-	      if (i != 98 || j != 99)
+		for (j = 0; j <= 99; j++)
 		{
-		  putchar(',');
-		  putchar(' ');
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
-	    }
 	}
-    }
-  putchar('\n');
-  return (0);
+	putchar('\n');
+	return (0);
 }
