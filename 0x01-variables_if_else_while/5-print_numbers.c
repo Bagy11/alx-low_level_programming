4631ac7b6938721d71963be9f135396a01b1c6f3
Bagy11 <122831906+Bagy11@users.noreply.github.com>
2023-02-18 00:42:40 +0000

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,16 +9,16 @@
  * Description: Prints numbers 0-9
  *
  * Return: 0 for success
- */
+*/
 int main(void)
 {
-  char num = '0';
+	char num = '0';
 
-  while (num <= '9')
-    {
-      putchar(num);
-      num++;
-    }
-  putchar('\n');
-  return (0);
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
 }
