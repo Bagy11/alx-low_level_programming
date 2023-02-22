4233ad39712b64e949c9da7af42401a0469c9324
Bagy11 <122831906+Bagy11@users.noreply.github.com>
2023-02-22 02:45:52 +0000

    103 fibonnaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,23 +8,23 @@
 
 int main(void)
 {
-  unsigned long x, y, z, sum;
+	unsigned long x, y, z, sum;
 
-  z = 0;
-  x = 0;
-  y = 1;
-  sum = 0;
+	z = 0;
+	x = 0;
+	y = 1;
+	sum = 0;
 
-  while (z < 4000000)
-    {
-      z = x + y;
-      x = y;
-      y = z;
+	while (z < 4000000)
+	{
+		z = x + y;
+		x = y;
+		y = z;
 
-      if (z % 2 == 0)
-	sum += z;
-    }
+		if (z % 2 == 0)
+			sum += z;
+	}
 
-  printf("%lu\n", sum);
-  return (0);
+	printf("%lu\n", sum);
+	return (0);
 }
