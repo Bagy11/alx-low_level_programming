5d932f0a25fb832ce07bde4a42710ff714492b99
Bagy11 <benedictyeboah35@yahoo.com>
2023-03-16 07:17:14 -0700

    advanced 1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,15 +10,15 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  unsigned int i;
+unsigned int i;
 
-  i = 0;
-  while (i < n)
-    {
-      dest[i] = src[i];
-      i++;
-    }
-  return (dest);
+i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
 }
 
 /**
